d79af6974e60978852418a6b5270c0ec5aa17308
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-04-07 14:13:13 +0300

    Updates

---
+++ precache-manifest.9efaf5c3adaa5e1583b01fb557f3cbf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01677b815c6708907b518aa224b9fba3",
+    "url": "/corona-search-app/index.html"
+  },
+  {
+    "revision": "07da3b7dd59696e9e25f",
+    "url": "/corona-search-app/static/css/main.48ec20f8.chunk.css"
+  },
+  {
+    "revision": "488c1cad4f1a2039910d",
+    "url": "/corona-search-app/static/js/2.d64a4c5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-search-app/static/js/2.d64a4c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07da3b7dd59696e9e25f",
+    "url": "/corona-search-app/static/js/main.16ead7ce.chunk.js"
+  },
+  {
+    "revision": "5519a827a1be5bb25ce1",
+    "url": "/corona-search-app/static/js/runtime-main.a76c3c34.js"
+  },
+  {
+    "revision": "323906f771b5ac17c8803e5f14272aee",
+    "url": "/corona-search-app/static/media/coronaImage.323906f7.png"
+  },
+  {
+    "revision": "01e8d8b3cd72dcbba863655dac044272",
+    "url": "/corona-search-app/static/media/goTop.01e8d8b3.png"
+  }
+]);
