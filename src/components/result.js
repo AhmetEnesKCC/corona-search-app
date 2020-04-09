bf07bde0e04105a90de8b8e96e869b49abc87a9d
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-04-09 03:00:35 +0300

    fixed when number passed million delete last 3 letter

--- src/components/result.js
+++ src/components/result.js
@@ -11,9 +11,9 @@ class Result extends React.Component {
             <div className="result">
                 {this.props.Flags && <img className="flags" src={this.props.Flags} alt="flags"/>}
                 <div className="left">
-                {this.props.Case && <p className="darker">Cases: <span className="yellow"><CountUp start={0} end={parseInt(this.props.Case.replace(",", ""))} duration={4}></CountUp></span></p>}
-                {this.props.Recovered && <p className="darker">Recovered: <span className="green"><CountUp start={0} end={parseInt(this.props.Recovered.replace(",", ""))} duration={4}></CountUp></span></p>}
-                {this.props.Deaths && <p className="darker">Deaths: <span className="red"><CountUp start={0} end={parseInt(this.props.Deaths.replace(",", ""))} duration={4}></CountUp></span></p>}
+                {this.props.Case && <p className="darker">Cases: <span className="yellow"><CountUp start={0} end={parseInt(this.props.Case.replace(/,/g, ""))} duration={4}></CountUp></span></p>}
+                {this.props.Recovered && <p className="darker">Recovered: <span className="green"><CountUp start={0} end={parseInt(this.props.Recovered.replace(/,/g, ""))} duration={4}></CountUp></span></p>}
+                {this.props.Deaths && <p className="darker">Deaths: <span className="red"><CountUp start={0} end={parseInt(this.props.Deaths.replace(/,/g, ""))} duration={4}></CountUp></span></p>}
                 {this.props.NewCases && <p className="darker">New cases: <span className="yellow">{this.props.NewCases}</span></p>}
                 {this.props.NewDeaths && <p className="darker">New deaths: <span id="newdeaths" className="red">{this.props.NewDeaths}</span></p>}
                 </div>
